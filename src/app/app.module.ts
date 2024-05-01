1eef0ebeac0312051c55d03a6a88a80635cd1d69
huajian123 <287643967@qq.com>
2024-05-01 19:50:23 +0800

    mf之前

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeComponent } from './me/me.component';
+import { ChildrenComponent } from './me/children/children.component';
+import {NzButtonComponent} from "ng-zorro-antd/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeComponent,
+    ChildrenComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NzButtonComponent
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/me/me.component.ts
@@ -0,0 +1,11 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-me',
+  templateUrl: './me.component.html',
+  styleUrl: './me.component.less',
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class MeComponent {
+
+}
